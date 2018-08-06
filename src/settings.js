d32fef2ad7e2f98be482bd0f303a79aad2775759
DESKTOP-VP4CSJ3\Admin <sy.divart@gmail.com>
2018-08-06 13:06:55 +0300

    Update settings

---
+++ src/settings.js
@@ -0,0 +1,7 @@
+const config = {
+	apiKey: 'your api key here',
+	authDomain: 'localhost', // website domain
+	databaseURL: 'database' // database url from firebase console
+}
+
+export default config
